12917142384309f3c6772c1bdff15ae89127d696
Brennon Nuckols <brennuck@gmail.com>
2020-02-25 10:55:07 -0700

    returning /

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
+const Router = require('./router.js');
+
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', Router)
+
+server.get('/', (req, res) => {
+    res.send('<h3>NODE</h3>');
+})
 
 module.exports = server;
