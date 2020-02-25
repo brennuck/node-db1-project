a310d2abe62769c2c06d32fd120719d635592539
Brennon Nuckols <brennuck@gmail.com>
2020-02-25 11:41:53 -0700

    postman won't work but this code should work

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const server = express();
 
 server.use(express.json());
 
-server.use('/api/accounts', Router)
-
 server.get('/', (req, res) => {
     res.send('<h3>NODE</h3>');
 })
+
+server.use('/accounts', Router);
 
 module.exports = server;
